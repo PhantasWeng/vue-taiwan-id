b6cb8093cb3b6d5b1509ef39fabd7b3e176c4bd8
Phantas Weng <bensonuong@gmail.com>
2020-08-25 15:37:59 +0800

    Add example code

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <!-- <input type="text" id="test" v-vueTaiwanId:aType.someBoolean="'身份證字號'" value=""/> -->
     <!-- <input type="text" value="" /> -->
     <hr>
+    <pre>
+      {{ exampleCode }}
+    </pre>
     <vueTaiwanId ref="testId" name="身份證字號" type="isNationalIdentificationNumberValid" @validated="onValidated" v-model="myId" />
     {{ result }}
   </div>
@@ -15,7 +18,8 @@ export default {
   data () {
     return {
       result: null,
-      myId: process.env.VUE_APP_TEST_ID
+      myId: process.env.VUE_APP_TEST_ID,
+      exampleCode: '<vueTaiwanId ref="testId" name="身份證字號" type="isNationalIdentificationNumberValid" @validated="onValidated" v-model="myId" />'
     }
   },
   methods: {
